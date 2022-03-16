ff97b838ee339c7c2aea8bd791d5183454e45dab
Michelle <michelle.lefebvre@valtech.com>
2022-03-16 10:03:22 -0400

    add ViewWorkout

--- src/views/ViewWorkout.vue
+++ src/views/ViewWorkout.vue
@@ -5,36 +5,206 @@
       <p class="text-at-light-green">{{ statusMsg }}</p>
       <p class="text-red-500">{{ errorMsg }}</p>
     </div>
-  </div>
 
-  <div v-if="dataLoaded">
-    <!-- General Workout Info -->
-    <div class="relative flex flex-col items-center p-8 rounded-md shadow-md bg-light-grey">
-      <div v-if="user" class="absolute flex left-2 top-2 gap-x-2">
-        <div
-          class="flex items-center justify-center rounded-full shadow-lg cursor-pointer h-7 w-7 bg-at-light-green"
-          @click="editMode"
-        >
-          <img class="h-3.5 w-auto" src="../assets/images/pencil-light.png" alt="to edit" />
+    <div v-if="dataLoaded">
+      <!-- General Workout Info -->
+      <div class="relative flex flex-col items-center p-8 rounded-md shadow-md bg-light-grey">
+        <div v-if="user" class="absolute flex left-2 top-2 gap-x-2">
+          <div
+            class="flex items-center justify-center rounded-full shadow-lg cursor-pointer h-7 w-7 bg-at-light-green"
+            @click="editMode"
+          >
+            <img class="h-3.5 w-auto" src="@/assets/images/pencil-light.png" alt />
+          </div>
+          <div
+            @click="deleteWorkout"
+            class="flex items-center justify-center rounded-full shadow-lg cursor-pointer h-7 w-7 bg-at-light-green"
+          >
+            <img class="h-3.5 w-auto" src="@/assets/images/trash-light.png" alt />
+          </div>
         </div>
-        <div
-          class="flex items-center justify-center rounded-full shadow-lg cursor-pointer h-7 w-7 bg-at-light-green"
-        >
-          <img class="h-3.5 w-auto" src="../assets/images/trash-light.png" alt="to delete" />
+
+        <img
+          v-if="data.workoutType === 'cardio'"
+          class="w-auto h-24"
+          src="@/assets/images/running-light-green.png"
+          alt
+        />
+
+        <img v-else class="w-auto h-24" src="@/assets/images/dumbbell-light-green.png" alt />
+
+        <span
+          class="mt-6 py-1.5 px-5 text-xs text-white bg-at-light-green rounded-lg shadow-md"
+        >{{ data.workoutType }}</span>
+
+        <div class="w-full mt-6">
+          <input
+            v-if="edit"
+            type="text"
+            class="w-full p-2 text-gray-500 focus:outline-none"
+            v-model="data.workoutName"
+          />
+          <h1 v-else class="text-2xl text-center text-at-light-green">{{ data.workoutName }}</h1>
         </div>
       </div>
-    </div>
 
-    <img class="w-auto h-24" src="../assets/images/running-light-green.png" alt />
+      <!-- Exercises -->
+      <div class="flex flex-col p-8 mt-10 rounded-md shadow-md item-center bg-light-grey">
+        <!-- Strength Training -->
+        <div v-if="data.workoutType === 'strength'" class="flex flex-col w-full gap-y-4">
+          <div
+            class="relative flex flex-col gap-x-6 gap-y-2 sm:flex-row"
+            v-for="(item, index) in data.exercises"
+            :key="index"
+          >
+            <div class="flex flex-col flex-2 md:w-1/3">
+              <label for="exercise-name" class="mb-1 text-sm text-at-light-green">Exercise</label>
+              <input
+                id="exercise-name"
+                v-if="edit"
+                class="w-full p-2 text-gray-500 focus:outline-none"
+                type="text"
+                v-model="item.exercise"
+              />
+              <p v-else>{{ item.exercise }}</p>
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="sets" class="mb-1 text-sm text-at-light-green">Sets</label>
+              <input
+                v-if="edit"
+                id="sets"
+                class="w-full p-2 text-gray-500 focus:outline-none"
+                type="text"
+                v-model="item.sets"
+              />
+              <p v-else>{{ item.sets }}</p>
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="reps" class="mb-1 text-sm text-at-light-green">Reps</label>
+              <input
+                v-if="edit"
+                id="reps"
+                class="w-full p-2 text-gray-500 focus:outline-none"
+                type="text"
+                v-model="item.reps"
+              />
+              <p v-else>{{ item.reps }}</p>
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="weight" class="mb-1 text-sm text-at-light-green">Weight (LB's)</label>
+              <input
+                v-if="edit"
+                id="weight"
+                class="w-full p-2 text-gray-500 focus:outline-none"
+                type="text"
+                v-model="item.weight"
+              />
+              <p v-else>{{ item.weight }}</p>
+            </div>
+            <img
+              v-if="edit"
+              @click="deleteExercise(item.id)"
+              class="absolute w-auto h-4 cursor-pointer -left-5"
+              src="@/assets/images/trash-light-green.png"
+              alt
+            />
+          </div>
+          <button
+            v-if="edit"
+            @click="addExercise"
+            type="button"
+            class="self-start px-6 py-2 text-sm text-white duration-200 border-2 border-transparent border-solid rounded-sm bg-at-light-green hover:border-at-light-green hover:bg-white hover:text-at-light-green"
+          >Add Exercise</button>
+        </div>
+
+        <!-- Cardio -->
+        <div v-else class="flex flex-col w-full gap-y-4">
+          <div
+            class="relative flex flex-col gap-x-6 gap-y-2 sm:flex-row"
+            v-for="(item, index) in data.exercises"
+            :key="index"
+          >
+            <div class="flex flex-col flex-2 md:w-1/3">
+              <label for="cardioType" class="mb-1 text-sm text-at-light-green">Type</label>
+              <select
+                id="cardioType"
+                v-if="edit"
+                class="w-full p-2 text-gray-500 focus:outline-none"
+                type="text"
+                v-model="item.cardioType"
+              >
+                <option value="#">Select Type</option>
+                <option value="run">Runs</option>
+                <option value="walk">Walk</option>
+              </select>
+              <p v-else>{{ item.cardioType }}</p>
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="distance" class="mb-1 text-sm text-at-light-green">Distance</label>
+              <input
+                v-if="edit"
+                id="distance"
+                class="w-full p-2 text-gray-500 focus:outline-none"
+                type="text"
+                v-model="item.distance"
+              />
+              <p v-else>{{ item.distance }}</p>
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="duration" class="mb-1 text-sm text-at-light-green">Duration</label>
+              <input
+                v-if="edit"
+                id="duration"
+                class="w-full p-2 text-gray-500 focus:outline-none"
+                type="text"
+                v-model="item.duration"
+              />
+              <p v-else>{{ item.duration }}</p>
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="pace" class="mb-1 text-sm text-at-light-green">Pace</label>
+              <input
+                v-if="edit"
+                id="pace"
+                class="w-full p-2 text-gray-500 focus:outline-none"
+                type="text"
+                v-model="item.pace"
+              />
+              <p v-else>{{ item.pace }}</p>
+            </div>
+            <img
+              @click="deleteExercise(item.id)"
+              v-if="edit"
+              class="absolute w-auto h-4 cursor-pointer -left-5"
+              src="@/assets/images/trash-light-green.png"
+              alt
+            />
+          </div>
+          <button
+            @click="addExercise"
+            v-if="edit"
+            type="button"
+            class="self-start px-6 py-2 text-sm text-white duration-200 border-2 border-transparent border-solid rounded-sm bg-at-light-green hover:border-at-light-green hover:bg-white hover:text-at-light-green"
+          >Add Exercise</button>
+        </div>
+      </div>
+
+      <!-- Update -->
+      <button
+        v-if="edit"
+        @click="update"
+        type="button"
+        class="self-start px-6 py-2 mt-10 text-sm text-white duration-200 border-2 border-transparent border-solid rounded-sm bg-at-light-green hover:border-at-light-green hover:bg-white hover:text-at-light-green"
+      >Update Workout</button>
+    </div>
   </div>
 </template>
 
 <script>
 import { ref, computed } from 'vue'
 import { supabase } from '../supabase/init';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import store from '../store/index';
-
 
 export default {
   name: "view-workout",
@@ -45,6 +215,7 @@ export default {
     const errorMsg = ref(null);
     const statusMsg = ref(null);
     const route = useRoute();
+    const router = useRouter();
     const user = computed(() => store.state.user)
 
     // Get current Id of route
@@ -54,10 +225,9 @@ export default {
       try {
         const { data: workouts, error } = await supabase.from('workouts').select('*').eq('id', currentId);
         if (error) throw error;
-        data.value = workouts;
+        data.value = workouts[0];
         dataLoaded.value = true;
       } catch (error) {
-        // console.warn(error.message)
         errorMsg.value = `Error: ${error.message}`;
         setTimeout(() => {
           errorMsg.value = false;
@@ -92,8 +262,9 @@ export default {
       data,
       dataLoaded,
       route,
+      edit,
       editMode,
-      user
+      user,
     };
   },
 };
